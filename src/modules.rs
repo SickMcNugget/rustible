7c78ab5fabd5cd2cbdf2308d5aa44c561c99e836
Joren <joren.regan@curtin.edu.au>
2025-05-21 17:13:58 +0800

    Change Error enum format

--- src/modules.rs
+++ src/modules.rs
@@ -2,26 +2,28 @@ pub mod add_host;
 pub mod apt;
 pub mod archive;
 pub mod git;
-pub mod read;
 
 pub type Result<T> = std::result::Result<T, ModuleError>;
 
 #[derive(Debug)]
 pub enum ModuleError {
-    Other,
+    PlainMessage(String),
     IOError(std::io::Error),
 }
 
 impl std::fmt::Display for ModuleError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "error in module")
+        match self {
+            Self::PlainMessage(msg) => write!(f, "{msg}"),
+            Self::IOError(e) => write!(f, "{e}"),
+        }
     }
 }
 
 impl std::error::Error for ModuleError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Self::Other => None,
+            Self::PlainMessage(_) => None,
             Self::IOError(ref err) => Some(err),
         }
     }
